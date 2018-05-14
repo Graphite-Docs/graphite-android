44e0b31f865fa252a3cd74b79ae005ad421acc81
Daniel-Wang <danielw68@gmail.com>
2018-05-13 21:14:50 -0400

    Add main activity and selector activity

---
+++ app/src/main/java/com/graphitedocs/graphitedocs/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.graphitedocs.graphitedocs
+
+import android.content.Intent
+import android.os.Bundle
+import org.blockstack.android.sdk.*
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_main.*
+
+import java.net.URI
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var session : BlockstackSession;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        setSupportActionBar(toolbar)
+
+
+        val appDomain = URI("https://graphitedocs.com")
+        val redirectURI = URI("${appDomain}/redirect")
+        val manifestURI = URI("${appDomain}/manifest.json")
+        val scopes = arrayOf("store_write")
+
+        session = BlockstackSession(this, appDomain, redirectURI, manifestURI, scopes,
+            onLoadedCallback = {
+            // Enable sign in your app
+            signin_button.isEnabled = true
+        })
+
+        session.redirectUserToSignIn { userData ->
+            // signed in!
+            Toast.makeText(this, "You are signed in", Toast.LENGTH_LONG).show();
+
+            // update your UI with signed in state
+            // runOnUiThread {
+            //     onSignIn(userData)
+            // }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent?.action == Intent.ACTION_VIEW) {
+            val response = intent?.dataString
+            if (response != null) {
+                val authResponseTokens = response.split(':')
+                if (authResponseTokens.size > 1) {
+                    val authResponse = authResponseTokens[1]
+                    val signInCallBack: (UserData) -> Unit = {
+                        Toast.makeText(this, "Pending sign in call", Toast.LENGTH_LONG).show();
+                    }
+                    session.handlePendingSignIn(authResponse, signInCallBack)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/graphitedocs/graphitedocs/SelectAppActivity.kt
@@ -0,0 +1,12 @@
+package com.graphitedocs.graphitedocs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_app)
+    }
+}
