fc1ef8e773afeef8b6a3ee45260bcc2a860d53f6
Daniel-Wang <danielw68@gmail.com>
2018-05-18 11:15:57 -0400

    Add changes to Selection page

--- app/src/main/java/com/graphitedocs/graphitedocs/SelectAppActivity.kt
+++ app/src/main/java/com/graphitedocs/graphitedocs/SelectAppActivity.kt
@@ -1,12 +1,28 @@
 package com.graphitedocs.graphitedocs
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.graphitedocs.graphitedocs.docs.DocsMainActivity
+import kotlinx.android.synthetic.main.activity_select_app.*
 
 class SelectAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_app)
+
+        documentsContainer.setOnClickListener {
+            val intent = Intent(this, DocsMainActivity::class.java)
+            startActivity(intent)
+        }
+
+        contactsContainer.setOnClickListener {
+
+        }
+
+        vaultContainer.setOnClickListener {
+
+        }
     }
 }
