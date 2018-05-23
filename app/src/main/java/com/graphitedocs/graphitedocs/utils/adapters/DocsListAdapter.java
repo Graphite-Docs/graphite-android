e4727df539530abc9093a066396be0f19140e2ec
Daniel-Wang <danielw68@gmail.com>
2018-05-23 11:15:31 -0400

    Add dagger dependencies

--- app/src/main/java/com/graphitedocs/graphitedocs/utils/adapters/DocsListAdapter.java
+++ app/src/main/java/com/graphitedocs/graphitedocs/utils/adapters/DocsListAdapter.java
@@ -1,11 +1,10 @@
-package utils.adapters;
+package com.graphitedocs.graphitedocs.utils.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.graphitedocs.graphitedocs.R;
 
