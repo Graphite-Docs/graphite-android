ff7c6f9ace65bef6cb120722ae123ce041fa327e
Daniel-Wang <danielw68@gmail.com>
2018-05-30 23:33:22 -0400

    Add DocsActivity

---
+++ app/src/main/java/com/graphitedocs/graphitedocs/docs/DocsActivity.kt
@@ -0,0 +1,13 @@
+package com.graphitedocs.graphitedocs.docs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.graphitedocs.graphitedocs.R
+
+class DocsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_docs)
+    }
+}
