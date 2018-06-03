dbe7b4dac8a2b3ffe03efa60aa01855f3f9cb6cd
Daniel-Wang <danielw68@gmail.com>
2018-06-03 18:54:21 -0400

    Add preview activity

--- app/src/main/java/com/graphitedocs/graphitedocs/docs/DocsActivity.kt
+++ app/src/main/java/com/graphitedocs/graphitedocs/docs/DocsActivity.kt
@@ -1,13 +1,127 @@
 package com.graphitedocs.graphitedocs.docs
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import com.graphitedocs.graphitedocs.R
+import kotlinx.android.synthetic.main.activity_docs.*
+import android.R.attr.duration
+import android.widget.Toast
+
+
 
 class DocsActivity : AppCompatActivity() {
+
+    var gestureDetector : GestureDetector? = null
+
+    var isPreview : Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_docs)
+
+        docsEditText.visibility = View.GONE
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            previewTextView.text = Html.fromHtml(getString(R.string.test), Html.FROM_HTML_MODE_COMPACT)
+        } else {
+            previewTextView.text = Html.fromHtml(getString(R.string.test))
+        }
+
+        gestureDetector = GestureDetector(this, object : GestureDetector.OnGestureListener {
+            override fun onShowPress(e: MotionEvent?) {
+            }
+
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                val actionbar = supportActionBar
+                val decorView = window.decorView
+
+                if (actionbar != null) {
+                    if (!actionbar.isShowing) {
+                        val uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                        decorView.systemUiVisibility = uiOptions
+
+                        actionbar.show()
+                        return true
+                    }
+                }
+                return false
+            }
+
+            override fun onDown(e: MotionEvent?): Boolean {
+                return true
+            }
+
+            override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+                return false
+            }
+
+            override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+                val actionbar = supportActionBar
+                val decorView = window.decorView
+                if (actionbar != null) {
+                    if (distanceY > 0) {
+                        if (actionbar.isShowing) {
+
+                            val uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+                            decorView.systemUiVisibility = uiOptions
+
+
+                            actionbar.hide()
+                            return true
+                        }
+                    } else {
+                        if (!actionbar.isShowing) {
+
+                            val uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                            decorView.systemUiVisibility = uiOptions
+
+                            actionbar.show()
+                            return true
+                        }
+                    }
+                }
+                return false
+            }
+
+            override fun onLongPress(e: MotionEvent?) {
+            }
+
+        })
+
+
+        previewScrollView.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v : View, event : MotionEvent) : Boolean {
+
+                return gestureDetector!!.onTouchEvent(event)
+            }
+        })
+
+    }
+
+
+    override fun onBackPressed() {
+        if (isPreview) {
+            super.onBackPressed()
+        } else {
+            // Change to preview mode
+        }
     }
 }
