b78b89e6ba38334215738dcbee1339a9f49f058f
Daniel-Wang <danielw68@gmail.com>
2018-06-21 13:53:51 -0400

    Get read file working for blockstak

--- app/src/main/java/com/graphitedocs/graphitedocs/docs/DocsListActivity.kt
+++ app/src/main/java/com/graphitedocs/graphitedocs/docs/DocsListActivity.kt
@@ -3,7 +3,6 @@ package com.graphitedocs.graphitedocs.docs
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import android.widget.Adapter
 import com.graphitedocs.graphitedocs.R
 import com.graphitedocs.graphitedocs.utils.GraphiteActivity
 import com.graphitedocs.graphitedocs.utils.adapters.DocsListAdapter
@@ -11,55 +10,42 @@ import com.graphitedocs.graphitedocs.utils.adapters.RecyclerSectionItemDecoratio
 import com.graphitedocs.graphitedocs.utils.models.DocsListItem
 import kotlinx.android.synthetic.main.activity_docs_main.*
 import org.blockstack.android.sdk.GetFileOptions
-import org.blockstack.android.sdk.PutFileOptions
-import java.net.URL
+import org.blockstack.android.sdk.UserData
 
 class DocsListActivity : GraphiteActivity() {
 
-    private var adapter: Adapter? = null
     private val TAG = DocsListActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_docs_main)
+    }
+
+    override fun onSignIn(userData: UserData) {
+        super.onSignIn(userData)
 
         rvDocs.layoutManager = LinearLayoutManager(this)
         // Get data
-        rvDocs.adapter = DocsListAdapter(this, getData())
 
-        val sectionItemDecoration = RecyclerSectionItemDecoration(resources.getDimensionPixelSize(R.dimen.docs_list_header), true, getSectionCallback(getData()))
+        var docsList = getData()
+        rvDocs.adapter = DocsListAdapter(this, docsList)
+
+        val sectionItemDecoration = RecyclerSectionItemDecoration(resources.getDimensionPixelSize(R.dimen.docs_list_header), true, getSectionCallback(docsList))
         rvDocs.addItemDecoration(sectionItemDecoration)
     }
 
     private fun getData () : ArrayList<DocsListItem> {
 
-//        val options = PutFileOptions()
-//        blockstackSession().putFile(getString(R.string.documents_list), "Hello Daniel!", options,
-//                {readURL: String ->
-//                    Log.d(TAG, "File stored at: ${readURL}")
-//                    runOnUiThread {
-//                        Log.d("Put file", "File stored at: ${readURL}")
-//                    }
-//                })
+        val options = GetFileOptions()
 
-        val options = PutFileOptions()
-        blockstackSession().putFile(getString(R.string.documents_list), "Hello Daniel!", options,
-                {readURL: String ->
-                    Log.d(TAG, "File stored at: ${readURL}")
-                    runOnUiThread {
-                    }
-                })
+        val fileName = getString(R.string.documents_list)
 
-//        val options = GetFileOptions()
-//
-//        val fileName = getString(R.string.documents_list)
-//
-//        blockstackSession().getFile(fileName, options, {content: Any ->
-//            // content can be a `String` or a `ByteArray`
-//            Log.d(TAG, content.toString())
-//
-//            // do something with `content`
-//        })
+        blockstackSession().getFile(fileName, options, {content: Any ->
+            // content can be a `String` or a `ByteArray`
+            Log.d(TAG, content.toString())
+
+            // do something with `content`
+        })
 
         // Create mock data for now
         var doc1 = DocsListItem("Daniel's Story", "May 9, 2018", listOf("danielwang.id"), listOf(""))
