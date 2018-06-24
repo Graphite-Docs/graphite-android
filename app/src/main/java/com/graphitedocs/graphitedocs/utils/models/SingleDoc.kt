045f8ea0d389145a4862dcaebb47c8c846a4cc8e
Daniel-Wang <danielw68@gmail.com>
2018-06-24 00:12:06 -0400

    Add SingleDoc model

---
+++ app/src/main/java/com/graphitedocs/graphitedocs/utils/models/SingleDoc.kt
@@ -0,0 +1,25 @@
+package com.graphitedocs.graphitedocs.utils.models
+
+import com.google.gson.GsonBuilder
+
+class SingleDoc(var title: String, var date: String, var sharedWith: List<String>?,
+                var tags: List<String>?, var author: String, var id: Long,
+                var updated: String?, var created: String?) {
+
+    companion object {
+        fun parseJSON(response: String): SingleDoc {
+            val DATE_FORMAT = "MM/dd/yyyy"
+
+            val gsonBuilder = GsonBuilder()
+            gsonBuilder.setDateFormat(DATE_FORMAT)
+            val gson = gsonBuilder.create()
+
+            var singleDoc: SingleDoc? = gson.fromJson(response, SingleDoc::class.java)
+
+            if (singleDoc == null) {
+                singleDoc = SingleDoc("Untitled", "", null, null, "", 0, "", "")
+            }
+            return singleDoc
+        }
+    }
+}
